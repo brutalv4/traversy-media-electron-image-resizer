24779bd69ee3b7aca3d36ce0a951ae56eb889851
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-12-28 10:03:26 +0200

    Create Main Window

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'renderer/image.html'));
+}
+
+async function main() {
+  await app.whenReady();
+  createMainWindow();
+}
+
+main();
