25ab7c39c5eab68a6392477b7a6eebcd130503ac
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-12-28 10:23:06 +0200

    Show devtools in dev mode

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
-function createMainWindow() {
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
-    width: 500,
+    width: isDev ? 1000: 500,
     height: 600,
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'renderer/image.html'));
-}
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
-async function main() {
+  mainWindow.loadFile(path.join(__dirname, 'renderer/image.html'));
+};
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length > 0) {
+    return;
+  }
+
+  createMainWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (isMac) {
+    return;
+  }
+  app.quit();
+});
+
+(async () => {
   await app.whenReady();
   createMainWindow();
-}
-
-main();
+})();
