473f90adcc51076251fcfa0d39600f666193b3f6
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-12-28 15:43:59 +0200

    Start Renderer/Load Image

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const form = document.querySelector('#img-form');
+const imgInput = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
+
+function loadImage({ target }) {
+  const [file] = target.files;
+  filename.innerText = file.name;
+  form.classList.remove('hidden');
+}
+
+imgInput.addEventListener('change', loadImage);
